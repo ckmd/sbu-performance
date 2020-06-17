400dc2419152f95e5420df2918e531953d80d52c
ckmd <rachmad.eepis@gmail.com>
2020-06-17 23:25:35 +0700

    add middleware auth

--- routes/web.php
+++ routes/web.php
@@ -20,4 +20,4 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
-Route::resource('rawdata','RawdataController');
+Route::resource('rawdata','RawdataController')->middleware('auth');
