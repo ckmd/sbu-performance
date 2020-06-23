d45929e41f1342b474c4c76e94f099c4b1e2bb1d
ckmd <rachmad.eepis@gmail.com>
2020-06-23 21:57:22 +0700

    create table above year to date chart

--- resources/views/dashboard/yearToDate.blade.php
+++ resources/views/dashboard/yearToDate.blade.php
@@ -4,3 +4,33 @@
         <canvas id="yearToDateChart"></canvas>
     </div>
 </div>
+<br>
+<table class="table table-hover table-bordered text-center">
+    <thead bgcolor="#eee">
+        <tr>
+            <th>&ensp;</th>
+            @foreach($bulanKe as $key => $value)
+            <th>{{$value}}</th>
+            @endforeach
+        </tr>
+    </thead>
+    <tr>
+        <td style="text-align:left">Nasional</td>
+        @foreach($nationalBulanVal as $key => $value)
+        <td>{{$value}}</td>
+        @endforeach
+    </tr>
+    <tr>
+        <td style="text-align:left">{{$sbu}}</td>
+        @foreach($bulanVal as $key => $value)
+        <td>{{$value}}</td>
+        @endforeach
+    </tr>
+    <tr>
+        <td style="text-align:left">KPI</td>
+        @foreach($kpiVal as $key => $value)
+        <td>{{$value}}</td>
+        @endforeach
+    </tr>
+</table>
+<br>
