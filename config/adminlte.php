6680ced642e82e2e5429d8bbe2678ab253132273
ckmd <rachmad.eepis@gmail.com>
2020-10-17 14:31:31 +0700

    change logo text to more perfect

--- config/adminlte.php
+++ config/adminlte.php
@@ -45,7 +45,7 @@ return [
     |
     */
 
-    'logo' => '<b>SBU</b> Performance',
+    'logo' => '<b> More Perfect </b>',
     'logo_img' => 'images/iconsquare.jpg',
     'logo_img_class' => 'brand-image img-circle elevation-3',
     'logo_img_xl' => null,
